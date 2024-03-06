33086d3f6b7fe68fadd00a0438d47a1d432f289d
JanPotat0 <jancquimpo@gmail.com>
2024-03-05 22:15:12 -0500

    added files for frontend skeleton

---
+++ MindEase_frontend/app.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+
+# Create users table if not exists
+with app.app_context():
+    db.create_all()
+    
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username, password=password).first()
+        if user:
+            return f'Welcome, {username}!'
+        else:
+            return 'Invalid username or password'
+    return render_template('login.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        email = request.form['email']
+        username = request.form['username']
+        password = request.form['password']
+        confirm_password = request.form['confirm_password']
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user:
+            return 'Username already exists'
+        elif password != confirm_password:
+            return 'Passwords do not match'
+        else:
+            new_user = User(email=email, username=username, password=password)
+            db.session.add(new_user)
+            db.session.commit()
+            return redirect(url_for('login'))
+    return render_template('register.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
